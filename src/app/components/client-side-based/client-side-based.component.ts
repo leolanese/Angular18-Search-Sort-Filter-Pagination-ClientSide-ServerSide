b8a0a1377e67179f95376ac56a1a496d05722f2c
leolanese <laneseleo@gmail.com>
2024-10-23 14:05:41 +0100

    Fibonacci Iteractive (loops)

--- src/app/components/client-side-based/client-side-based.component.ts
+++ src/app/components/client-side-based/client-side-based.component.ts
@@ -1,193 +1,143 @@
-import {DataSource} from '@angular/cdk/collections';
 import {CommonModule} from '@angular/common';
-import {AfterViewInit,Component,OnInit,ViewChild,inject} from '@angular/core';
-import {FormControl,FormsModule,ReactiveFormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCard} from '@angular/material/card';
-import {MatFormField,MatFormFieldModule,MatLabel} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatPaginator,MatPaginatorModule} from '@angular/material/paginator';
+import {Component,inject,OnInit} from '@angular/core';
+import {FormBuilder,FormControl,ReactiveFormsModule} from '@angular/forms';
+import {MatPaginatorModule,PageEvent} from '@angular/material/paginator';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatSort,MatSortModule} from '@angular/material/sort';
-import {MatTableModule} from '@angular/material/table';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {BehaviorSubject,Observable,debounceTime,distinctUntilChanged,map,merge,startWith} from 'rxjs';
-import {ResponseItem} from '../../models/api.module';
-import {ClientSideBasedPaginationService} from '../../services/client.side.based.pagination.service';
+import {Sort} from '@angular/material/sort';
+import {MatTableDataSource,MatTableModule} from '@angular/material/table';
+import {debounceTime,distinctUntilChanged,startWith,switchMap} from 'rxjs/operators';
 
-class CustomDataSource extends DataSource<ResponseItem> {
-  private dataSubject = new BehaviorSubject<ResponseItem[]>([]);
-  private loadingSubject = new BehaviorSubject<boolean>(true);
-
-  loading$ = this.loadingSubject.asObservable();
-
-  constructor(private clientSideBasedPaginationService: ClientSideBasedPaginationService) {
-    super();
-    this.loadData();
-  }
-
-  loadData() {
-    this.clientSideBasedPaginationService.fetchTableData().subscribe(
-      response => {
-        this.dataSubject.next(response.items);
-        this.loadingSubject.next(false);
-      }
-    );
-  }
-
-  connect(): Observable<ResponseItem[]> {
-    return this.dataSubject.asObservable();
-  }
-
-  disconnect() {
-    this.dataSubject.complete();
-    this.loadingSubject.complete();
-  }
-}
-
-function compare(a: string | number, b: string | number, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-}
+import {MatCard} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {ApiRequest,ClientSideBasedPaginationService,ResponseItem} from '../../services/client.side.based.pagination.service';
 
 @Component({
   selector: 'app-client-side-based',
   standalone: true,
+  template: `
+    <div *ngIf="isLoading" class="loading-indicator">
+      <mat-spinner></mat-spinner>
+    </div>
+
+    <mat-card>
+      <mat-form-field class="search">
+        <mat-label>Search (using client-side search)</mat-label>
+        <input matInput [formControl]="searchKeywordFilter" placeholder="Title to Search">
+      </mat-form-field>
+
+      <div class="table-wrapper">
+        <table mat-table [dataSource]="dataSource" matSort matSortDirection="desc" class="mat-elevation-z8">
+        
+          <ng-container matColumnDef="id">
+            <th mat-header-cell *matHeaderCellDef mat-sort-header>ID</th>
+            <td mat-cell *matCellDef="let row">{{ row.number }}</td>
+          </ng-container>
+
+          <ng-container matColumnDef="title">
+            <th mat-header-cell *matHeaderCellDef mat-sort-header>Title</th>
+            <td mat-cell *matCellDef="let row">{{ row.title }}</td>
+          </ng-container>
+
+          <ng-container matColumnDef="state">
+            <th mat-header-cell *matHeaderCellDef mat-sort-header>State</th>
+            <td mat-cell *matCellDef="let row">{{ row.state }}</td>
+          </ng-container>
+
+          <ng-container matColumnDef="created">
+            <th mat-header-cell *matHeaderCellDef mat-sort-header>Created</th>
+            <td mat-cell *matCellDef="let row">{{ row.created_at | date }}</td>
+          </ng-container>
+
+          <tr mat-header-row *matHeaderRowDef="columnsToDisplay"></tr>
+          <tr mat-row *matRowDef="let row; columns: columnsToDisplay"></tr>
+        </table>
+
+        <mat-paginator [length]="totalCount" [pageSizeOptions]="pageSizes"
+          aria-label="Select page" showFirstLastButtons (page)="onPageChange($event)">
+        </mat-paginator>
+        
+      </div>
+    </mat-card>
+  `,
   imports: [
-    CommonModule, 
-    FormsModule, ReactiveFormsModule,
-    MatCard, MatFormField, MatLabel, MatTableModule, MatFormFieldModule,
-    MatSortModule, MatPaginatorModule, MatSelectModule, MatInputModule, MatButtonModule,
-    MatTooltipModule, MatSnackBarModule, MatProgressSpinnerModule
-  ],
-  templateUrl: './client-side-based.component.html',
-  styleUrl: './client-side-based.component.scss'
+    CommonModule, ReactiveFormsModule, 
+    MatProgressSpinnerModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatCard,
+    MatFormFieldModule,
+    MatTableModule
+  ]
 })
-export class ClientSideBasedComponent implements OnInit, AfterViewInit {
-  private clientSideBasedPaginationService = inject(ClientSideBasedPaginationService)
-
-  columnsToDisplay: string[] = ['title', 'number', 'created', 'state'];
-  pageSizes = [5, 10, 20];
-  isLoading = true;
-  currentPage = 1;
-
-  dataSource: CustomDataSource;
-  private isUpdating = false; // Flag to prevent infinite loop
-
-  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
-  @ViewChild(MatSort, { static: false }) sort!: MatSort;
-
+export class ClientSideBasedComponent implements OnInit {
   searchKeywordFilter = new FormControl('');
+  dataSource = new MatTableDataSource<ResponseItem>();
+  isLoading = false;
+  totalCount = 0;
+  currentPage = 0;
+  pageSize = 3; 
+  sortOrder: 'asc' | 'desc' = 'asc';
+  sortField = 'created_at';
+  pageSizes = [3, 5, 10];
+  columnsToDisplay = ['id', 'title', 'state', 'created'];
 
-  constructor() {
-    this.dataSource = new CustomDataSource(this.clientSideBasedPaginationService);
-  }
+  private paginationService = inject(ClientSideBasedPaginationService);
+  private fb = inject(FormBuilder);
 
-  ngOnInit(): void {
-    // The data loading is now handled by the CustomDataSource
-    this.dataSource.loading$.subscribe(isLoading => {
-      this.isLoading = isLoading;
-    });
-  }
-
-  ngAfterViewInit() {
-    this.setupSorting();
-    this.setupFiltering();
-    this.setupPagination();
-  }
-
-  private setupSorting() {
-    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
-    
-    merge(this.sort.sortChange, this.paginator.page)
-      .pipe(
-        startWith({}),
-        map(() => this.getSortedData([...this.dataSource['dataSubject'].value]))
-      )
-      .subscribe(data => { 
-        this.dataSource['dataSubject'].next(data);
-      });
-  }
-
-  private setupFiltering() {
-    this.searchKeywordFilter.valueChanges
-      .pipe(
-        debounceTime(300),
-        distinctUntilChanged(),
-        map(value => this.filterData(value))
-      )
-      .subscribe(filteredData => {
-        this.paginator.pageIndex = 0;
-        this.dataSource['dataSubject'].next(filteredData);
-      });
-  }
-
-  private setupPagination() {
-    this.paginator.page
-      .pipe(
-        startWith({}),
-        map(() => {
-          const data = this.dataSource['dataSubject'].value;
-          const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
-
-          console.log( this.pageSizes )
-
-          return data.slice(startIndex, startIndex + this.paginator.pageSize);
-        })
-      )
-      .subscribe(paginatedData => { 
-        this.updatePaginatedData(paginatedData);
-      });
-
-    // this.paginator.pageSize = 5;
-    this.dataSource.connect().pipe(
-      map(data => {
-        const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
-        return data.slice(startIndex, startIndex + this.paginator.pageSize);
+  ngOnInit() {
+    this.searchKeywordFilter.valueChanges.pipe(
+      startWith(''),
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(filterValue => {
+        this.isLoading = true;
+        const payload: ApiRequest = {
+          search_filter: filterValue || '',
+          sort_field: this.sortField,
+          sort_order: this.sortOrder,
+          page: this.currentPage + 1,
+          limit_per_page: this.pageSize
+        };
+        return this.paginationService.fetchTableData(payload);
       })
-    ).subscribe(paginatedData => {
-      this.updatePaginatedData(paginatedData);
+    ).subscribe(response => {
+      this.dataSource.data = response.items;
+      this.totalCount = response.total_count;
+      this.isLoading = false;
+    }, () => {
+      this.isLoading = false;
     });
   }
 
-  private updatePaginatedData(paginatedData: ResponseItem[]) {
-    // Update the data subject with the paginated data
-    if (!this.isUpdating) {
-      this.isUpdating = true;
-      // Update the data subject with the paginated data
-      this.dataSource['dataSubject'].next(paginatedData);
-      this.isUpdating = false;
-    }
+  sortData(sort: Sort) {
+    this.sortField = sort.active;
+    this.sortOrder = sort.direction || 'asc';
+    this.currentPage = 0; // Reset to the first page on sort change
+    this.fetchData();
   }
 
-  private getSortedData(data: ResponseItem[]): ResponseItem[] {
-    if (!this.sort.active || this.sort.direction === '') {
-      return data;
-    }
+  onPageChange(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
+    this.fetchData();
+  }
 
-    return data.sort((a, b) => {
-      const isAsc = this.sort.direction === 'asc';
-      switch (this.sort.active) {
-        case 'title': return compare(a.title, b.title, isAsc);
-        case 'number': return compare(a.number, b.number, isAsc);
-        case 'created': return compare(a.created_at, b.created_at, isAsc);
-        case 'state': return compare(a.state, b.state, isAsc);
-        default: return 0;
-      }
+  private fetchData() {
+    this.isLoading = true;
+    const payload: ApiRequest = {
+      search_filter: this.searchKeywordFilter.value || '',
+      sort_field: this.sortField,
+      sort_order: this.sortOrder,
+      page: this.currentPage + 1,
+      limit_per_page: this.pageSize
+    };
+    this.paginationService.fetchTableData(payload).subscribe(response => {
+      this.dataSource.data = response.items;
+      this.totalCount = response.total_count;
+      this.isLoading = false;
+    }, () => {
+      this.isLoading = false;
     });
   }
-
-  private filterData(filterValue: any): ResponseItem[] {
-    return this.dataSource['dataSubject'].value.filter(item =>
-      item.title.toLowerCase().includes(filterValue.toLowerCase())
-    );
-  }
-
-  onPageChange(page: any): void {
-    this.currentPage = page;
-    this.setupPagination();
-  }
-
-
 }
--- src/app/services/client.side.based.pagination.service.ts
+++ src/app/services/client.side.based.pagination.service.ts
@@ -1,6 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {inject,Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {SortDirection} from '@angular/material/sort';
+import {map,Observable} from 'rxjs';
 
 export interface ApiResponse {
     items: ResponseItem[];
@@ -14,20 +15,36 @@ export interface ApiResponse {
     title: string;
   }
 
-@Injectable({
-  providedIn: 'root',
-})
+  export interface ApiRequest {
+    search_filter: string,
+    sort_field: string,
+    sort_order: SortDirection,
+    page: number,
+    limit_per_page: number
+  }
+
+@Injectable({providedIn: 'root'})
 export class ClientSideBasedPaginationService {
   http = inject(HttpClient);
   
   constructor() {}
 
-  fetchTableData(): Observable<ApiResponse> {
+  fetchTableData(payload: ApiRequest): Observable<ApiResponse> {
     const url = 'https://api.github.com/search/issues';
-    const requestUrl = `${url}?q=repo:angular/components&per_page=100`;
+    const requestUrl = `${url}?q=repo:angular/components`;
     
     
-    return this.http.get<ApiResponse>(requestUrl)
+    return this.http.get<ApiResponse>(requestUrl).pipe(
+      map(response => ({
+        items: response.items.map(item => ({
+          created_at: item.created_at,
+          number: item.number.toString(),
+          state: item.state,
+          title: item.title
+        })),
+        total_count: response.total_count
+      }))
+    );
   }
 
 }
